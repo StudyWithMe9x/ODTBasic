f6fb5e1fa5d787171f38925221591fe871cbcfd3
StudyWithMe9x <remoteathena@gmail.com>
2025-03-30 04:36:36 -0700

    Update installOffice365.ps1

--- installOffice365.ps1
+++ installOffice365.ps1
@@ -7,4 +7,10 @@ $exePackageToInstall = "$Path\setup.exe"
 $arguments = "/configure $Path\ODTBasic.xml" 
 
 Start-Process -FilePath $exePackageToInstall -ArgumentList $arguments -Wait
-exit
+
+$Path = "$($env:USERPROFILE)\Downloads"; $Installer = "active.zip"; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip" -OutFile $Path\$Installer;
+Expand-Archive $Path\active.zip -DestinationPath $Path\active
+
+
+start $Path\active\Microsoft-Activation-Scripts-master\MAS\All-In-One-Version-KL\MAS_AIO.cmd
+
